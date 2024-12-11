d518dc26e292268ac12982277968386fcdc14695
MasRama <ryushineo@gmail.com>
2024-12-11 14:46:31 +0700

    fix icon

--- resources/js/Components/CalculateHistory.svelte
+++ resources/js/Components/CalculateHistory.svelte
@@ -226,10 +226,18 @@
                     Edit
                   </button>
                   {#if calc.exit_weight}
-                    <span class="inline-flex items-center justify-center w-6 h-6 bg-green-100 text-green-500 rounded-full relative group">
-                      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
-                      </svg>
+                    <span class="inline-flex items-center justify-center w-6 h-6 bg-green-50 text-green-500 rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                          </svg>
+                          
+                          
+                    </span>
+                  {:else}
+                    <span class="inline-flex items-center justify-center w-6 h-6 bg-yellow-50 text-yellow-500 rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
+                          </svg>
                     </span>
                   {/if}
                 </td>
